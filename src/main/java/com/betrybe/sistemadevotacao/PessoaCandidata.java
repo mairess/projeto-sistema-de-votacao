2049e631236f7f217602da3fc56cec490fb975cd
mairess <trabalho.maires@gmail.com>
2024-05-08 20:14:11 -0300

    feat: implements class PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -3,6 +3,61 @@ package com.betrybe.sistemadevotacao;
 /**
  * The type Pessoa candidata.
  */
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
 
+  /**
+   * The Numero.
+   */
+  int numero;
+  /**
+   * The Votos.
+   */
+  int votos;
+
+  /**
+   * Instantiates a new Pessoa candidata.
+   *
+   * @param nome   the nome
+   * @param numero the numero
+   */
+  public PessoaCandidata(String nome, int numero) {
+    super();
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  /**
+   * Gets numero.
+   *
+   * @return the numero
+   */
+  public int getNumero() {
+    return numero;
+  }
+
+  /**
+   * Sets numero.
+   *
+   * @param numero the numero
+   */
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  /**
+   * Gets votos.
+   *
+   * @return the votos
+   */
+  public int getVotos() {
+    return votos;
+  }
+
+  /**
+   * Receber voto.
+   */
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
